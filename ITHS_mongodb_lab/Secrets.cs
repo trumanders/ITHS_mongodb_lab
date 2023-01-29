3927b9fe99805a055623c7156ec6f539555e15cc
Anders <trumanders@gmail.com>
2023-01-29 18:19:33 +0100

    Update Secrets.cs

--- ITHS_mongodb_lab/Secrets.cs
+++ ITHS_mongodb_lab/Secrets.cs
@@ -2,6 +2,6 @@
 
 public class Secrets
 {
-    public static string clusterConnectionString = "mongodb+srv://trumanders:Threeand4@cluster0.0jtojpb.mongodb.net/?retryWrites=true&w=majority";
+    public static string clusterConnectionString = "mongodb+srv://trumanders:PASSWORD@cluster0.0jtojpb.mongodb.net/?retryWrites=true&w=majority";
     
 }
